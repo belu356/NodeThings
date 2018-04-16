c83369b4468d8b5f24e5e95e520adefd40d6157a
Moreno, Belen E <belen.e.moreno@accenture.com>
2018-04-16 18:08:29 -0300

    fixs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     mongoose = require('mongoose');
-    //bodyParser = require('body-parser');
+    bodyParser = require('body-parser');
 
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
@@ -11,21 +11,21 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-//app.use(bodyParser.urlencoded({extended: true})); //bodyp loade, mira el body 
-//app.use(bodyParser.json()); //parsea a json
+app.use(bodyParser.urlencoded({extended: true})); //bodyp loade, mira el body 
+app.use(bodyParser.json()); //parsea a json
 
 
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
-    /*.post(function(req, res){
+    .post(function(req, res){
         var book = new Book(req.body);
 
         book.save();
         console.log(book);
         res.status(201).send(book);
 
-    })*/
+    })
     .get(function (req, res) {
 
         var query = {};
@@ -43,7 +43,7 @@ bookRouter.route('/Books')
 bookRouter.route('/Books/:bookId')
     .get(function(req, res) {
 
-        Book.findById(req.params.bookId, function (err, book) {
+        Book.findById(req.params.bookId, function (err, book) {  
             if (err)
                 res.status(500).send(err);
             else
