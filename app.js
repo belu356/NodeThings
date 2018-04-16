408fb528fd5222e5359bf577cd22e24123a37f2c
Moreno, Belen E <belen.e.moreno@accenture.com>
2018-04-16 16:56:50 -0300

    la maldita S

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     mongoose = require('mongoose');
-    bodyParser = require('body-parser')
+    //bodyParser = require('body-parser');
 
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
@@ -11,26 +11,26 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true})); //bodyp loade, mira el body 
-app.use(bodyParser.json()); //parsea a json
+//app.use(bodyParser.urlencoded({extended: true})); //bodyp loade, mira el body 
+//app.use(bodyParser.json()); //parsea a json
 
 
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
-.post(function(req, res){
-    var book = new Book(req.body);
+    /*.post(function(req, res){
+        var book = new Book(req.body);
 
-    book.save();
-    console.log(book);
-    res.status(201).send(book);
+        book.save();
+        console.log(book);
+        res.status(201).send(book);
 
-})
+    })*/
     .get(function (req, res) {
 
         var query = {};
         if (req.query.genre) {
-            query.genre = req.query.genre
+            query.genre = req.query.genre;
         }
         Book.find(query, function (err, books) {
             if (err)
@@ -40,10 +40,10 @@ bookRouter.route('/Books')
         });
     });
 
-bookRouter.route('/Book/:bookId')
-    .get(function (req, res) {
+bookRouter.route('/Books/:bookId')
+    .get(function(req, res) {
 
-        Book.findById(req.params.bookId, function (err, books) {
+        Book.findById(req.params.bookId, function (err, book) {
             if (err)
                 res.status(500).send(err);
             else
